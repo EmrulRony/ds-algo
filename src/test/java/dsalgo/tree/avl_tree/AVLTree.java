e431a3cb43e2e92fee56c301d9bb9bac76f2ff04
Emrul Rony <emrulhasanrony@gmail.com>
2021-02-18 02:43:42 +0600

    AVL tree insertion LL, LR, RR, RL

--- src/test/java/dsalgo/tree/avl_tree/AVLTree.java
+++ src/test/java/dsalgo/tree/avl_tree/AVLTree.java
@@ -5,6 +5,15 @@ import dsalgo.tree.node.TreeNode;
 import java.util.LinkedList;
 import java.util.Queue;
 
+//T1, T2, T3 and T4 are subtrees.
+//        z                                        y
+//        / \                                    /    \
+//        y   T4      Right Rotate (z)          x      z
+//        / \          - - - - - - - - ->      /  \    / \
+//        x   T3                               T1  T2  T3 T4
+//        / \
+//        T1   T2
+
 public class AVLTree {
     TreeNode root;
 
@@ -26,16 +35,35 @@ public class AVLTree {
         }
         root.height = Math.max(getNodeHeight(root.left), getNodeHeight(root.right)) + 1;
         int balanceFactor = Math.subtractExact(getNodeHeight(root.left),getNodeHeight(root.right));
-        if (balanceFactor > 1 && root.value > value){
-            root = leftLeftRotate(root);
+        if (balanceFactor > 1 && root.left.value > value){
+            root = rightRotate(root);
+        } else if (balanceFactor > 1 && root.left.value < value){
+            root.left = leftRotate(root.left);
+            root = rightRotate(root);
+        } else if (balanceFactor < -1 && value > root.right.value){
+            root = leftRotate(root);
+        } else if (balanceFactor < -1 && value < root.right.value){
+            root.right = rightRotate(root.right);
+            root = leftRotate(root);
         }
         return root;
     }
 
-    private TreeNode leftLeftRotate(TreeNode currNode) {
+    private TreeNode leftRotate(TreeNode currNode) {
+        TreeNode newRoot = currNode.right;
+        currNode.right = currNode.right.left;
+        newRoot.left = currNode;
+        newRoot.height = Math.max(getNodeHeight(newRoot.left), getNodeHeight(newRoot.right));
+        currNode.height = Math.max(getNodeHeight(currNode.left), getNodeHeight(currNode.right));
+        return newRoot;
+    }
+
+    private TreeNode rightRotate(TreeNode currNode) {
         TreeNode newRoot = currNode.left;
         currNode.left = currNode.left.right;
         newRoot.right = currNode;
+        newRoot.height = Math.max(getNodeHeight(newRoot.left), getNodeHeight(newRoot.right));
+        currNode.height = Math.max(getNodeHeight(currNode.left), getNodeHeight(currNode.right));
         return newRoot;
     }
 

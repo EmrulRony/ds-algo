ec196e36b47759f4ba8cad85f4ab737051eb3732
emrulrony <ronyemrul@gmail.com>
2022-03-31 23:59:13 +0600

    First non repeating character

---
+++ src/test/java/algoexpert/string/firstNonRepeatingCharacter/Program.java
@@ -0,0 +1,22 @@
+package algoexpert.string.firstNonRepeatingCharacter;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+class Program {
+
+    public int firstNonRepeatingCharacter(String string) {
+        // Write your code here.
+        Map<Character, Integer> charCountMap = new HashMap<>();
+        for (int i = 0; i < string.length(); i++) {
+            charCountMap.put(string.charAt(i), charCountMap.getOrDefault(string.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < string.length(); i++) {
+            if (charCountMap.get(string.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}

1f3b1366998d721096f877b867f76b13bb6d20cc
emrulrony <ronyemrul@gmail.com>
2022-04-20 23:57:12 +0600

    Linked list: find the loop

--- src/test/java/algoexpert/linkedList/findTheLoop/Program.java
+++ src/test/java/algoexpert/linkedList/findTheLoop/Program.java
@@ -3,7 +3,18 @@ package algoexpert.linkedList.findTheLoop;
 public class Program {
     public static LinkedList findLoop(LinkedList head) {
         // Write your code here.
-        return null;
+        LinkedList fast = head.next.next;
+        LinkedList slow = head.next;
+        while (fast != slow) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
     }
 
     static class LinkedList {
